4f4f4a777c365dd4ecaeb0d77a86b0323657e7c8
Aparajita Bandyopadhyay <117743177+Aparajita976@users.noreply.github.com>
2023-04-07 10:41:26 +0530

    Node server for Blog app

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-//import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
+const __dirname = path.resolve();
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = dirname(__filename);
+import dotenv from 'dotenv';
 const app = express();
 
 import userRouter from './routes/user-routes';
 import blogRouter from './routes/blog-routes';
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+/*app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});*/
+app.use(cors({ credentials: true, origin: "*" }));
 app.use(express.json());
-//app.use(cors);
+app.use(express.static(path.join(__dirname, './frontend/build')))
+
 app.use("/user", userRouter);
 app.use("/blog", blogRouter);
-//dotenv.config();
+dotenv.config();
 mongoose.connect('mongodb+srv://admin:lTgFmfLTH7RrdNYe@cluster0.4ruf5ba.mongodb.net/?retryWrites=true&w=majority').then(() => console.log("DBconnection successfull")).catch((error) => { console.log("error") });
-
-app.listen(5000, () => {
+app.use('*', function (req, res) {
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'))
+})
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
     console.log("server is running");
 })
 
